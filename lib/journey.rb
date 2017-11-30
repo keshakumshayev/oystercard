e5b455678ceee1d6f8e87014d31dabe355791069
kkumshayev <kumshkesh@gmail.com>
2017-11-30 13:39:23 +0000

    passing tests for journey methods

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,8 +2,8 @@ class Journey
   attr_reader :entry_station, :exit_station
 
   def initialize
-    @entry_station = entry_station
-    @exit_station = exit_station
+    @entry_station = nil
+    @exit_station = nil
   end
 
   def start(station)
